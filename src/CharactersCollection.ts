71c33cb03180e8224a6f962602e767d01f1d71e5
jonascolas <jonas@fusemachines.com>
2022-05-25 20:18:34 -0300

    linked list sorted

--- src/CharactersCollection.ts
+++ src/CharactersCollection.ts
@@ -17,5 +17,4 @@ export class CharactersCollection {
 
     this.data = characters.join('');
   }
-
 }
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { Sorter } from "./Sorter";
 import { NumbersCollection } from "./NumbersCollection";
 import { CharactersCollection } from "./CharactersCollection";
+import { LinkedList } from "./LinkedList";
 
 const numbersCollection = new NumbersCollection([89, 7, 10, 3, -5, 0]);
 const sorter = new Sorter(numbersCollection);
@@ -11,3 +12,17 @@ const charsCollection = new CharactersCollection("XyzCaadasd");
 const sorterChars = new Sorter(charsCollection);
 sorterChars.sort();
 console.log(charsCollection.data);
+
+const linkedList = new LinkedList();
+linkedList.add(500);
+linkedList.add(-10);
+linkedList.add(-3);
+linkedList.add(4);
+linkedList.add(2);
+linkedList.add(8);
+linkedList.add(1);
+linkedList.add(3);
+
+const sorterLink = new Sorter(linkedList);
+sorterLink.sort();
+linkedList.print();
